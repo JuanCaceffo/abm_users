b300f0821f9023eab7deac96a721eb56b597ce83
JuanCaceffo <caceffojuan@gmail.com>
2024-09-29 17:45:55 -0300

    agregado de typado de usuario + mockeo de la data del mismo

---
+++ abm-users/src/mocks/userMock.ts
@@ -0,0 +1,64 @@
+import { UserData } from "src/types/userTypes";
+
+export const userMockedData: UserData = [
+  {
+    "usuario": "STAR OVER SRL",
+    "estado": "INACTIVO",
+    "sector": 2222,
+    "id": 934631
+  },
+  {
+    "id": 934632,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "ADMINGSC"
+  },
+  {
+    "id": 934633,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "AS"
+  },
+  {
+    "id": 934634,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "OLIVETI"
+  },
+  {
+    "id": 934635,
+    "estado": "INACTIVO",
+    "sector": 2222,
+    "usuario": "GOMEZ"
+  },
+  {
+    "id": 934636,
+    "estado": "INACTIVO",
+    "sector": 2222,
+    "usuario": "REINA"
+  },
+  {
+    "id": 934637,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "ADMINCHELA"
+  },
+  {
+    "id": 934638,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "CENTURION"
+  },
+  {
+    "id": 934639,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "CUERVA"
+  },
+  {
+    "id": 934640,
+    "estado": "ACTIVO",
+    "sector": 2222,
+    "usuario": "DIAZ"
+  }
+]
---
+++ abm-users/src/types/userTypes.ts
@@ -0,0 +1,6 @@
+export type UserData = {
+  id: number,
+  username: string,
+  state: string,
+  sector: string
+}
