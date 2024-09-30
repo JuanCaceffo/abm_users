6173a23e95648d4a22fae63b286b27e62a4cf896
JuanCaceffo <caceffojuan@gmail.com>
2024-09-29 21:28:48 -0300

    agregado de service de usaurio

--- abm-users/src/services/UserService.ts
+++ abm-users/src/services/UserService.ts
@@ -1,15 +1,13 @@
 import { userMockedData } from "src/mocks/userMock"
 import { User } from "src/models/User"
 
-export const userService = () => {
+class UserService {
 
   //TODO: agregar llamado a API
-  const getUserData: () => Array<User> = () => {
+  getUserData(): Array<User> {  
     return userMockedData.map((userData) => (new User(userData)))
   }
 
-  return {
-    getUserData
-  }
 }
 
+export const userService = new UserService()
