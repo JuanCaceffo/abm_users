59f11d4d1150de65223e4da6af2d5775f0bb82ad
JuanCaceffo <caceffojuan@gmail.com>
2024-10-01 16:46:47 -0300

    user table added in page

--- abm-users/src/app/page.tsx
+++ abm-users/src/app/page.tsx
@@ -1,8 +1,11 @@
+import UserTable from "./atoms/userTable/userTable";
 import styles from "./page.module.css";
+import { userService } from "src/services/UserService";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <UserTable values={userService.getUserData()}/>
     </div>
   );
 }
