649640965b0dea861b4533c195d12666d0aaa9a9
JuanCaceffo <caceffojuan@gmail.com>
2024-10-03 18:08:28 -0300

    FEAT: added fixed sector param in 7000

--- abm-users/src/services/config/httpClient.ts
+++ abm-users/src/services/config/httpClient.ts
@@ -3,6 +3,9 @@ import axios from 'axios'
 //TODO: manejar el URL de la API con variables de entorno
 const httpClient = axios.create({
   baseURL: 'http://localhost:3000',
+  params: {
+    sector: '7000'
+  }
 })
 
 export default httpClient
