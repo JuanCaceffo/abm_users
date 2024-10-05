f63e122556e542828a5b9006792fbb3b775db59a
JuanCaceffo <caceffojuan@gmail.com>
2024-10-04 21:31:05 -0300

    FEAT: better styling for the filter

--- abm-users/src/app/molecules/filter/filter.tsx
+++ abm-users/src/app/molecules/filter/filter.tsx
@@ -1,65 +1,85 @@
 'use client'
 //Libraries
 import { FC, useState } from 'react'
-import Link from 'next/link';
-import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
-import { IconField } from 'primereact/iconfield';
-import { InputIcon } from 'primereact/inputicon';
-import { InputText } from 'primereact/inputtext';
+import Link from 'next/link'
+import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
 //Styles
 import styles from './filter.module.css'
 //Components
-import IconButton from 'src/app/atoms/iconButton/iconButton';
+import BaseButton from 'src/app/atoms/baseButton/baseButton'
 //Types
-import { filterData, stateT } from 'src/types/filetTypes';
+import { filterData, stateT } from 'src/types/filetTypes'
 
 const Filter: FC = () => {
+  const [filterState, setFilterState] = useState<filterData>({})
+  const { username, state } = filterState
 
-  const [filterState, setFilterState] = useState<filterData>({}) 
-  const {username, state} = filterState 
-
-  const optEstado: stateT[] = [
-    'ACTIVO',
-    'INACTIVO'
-  ]
+  const optEstado: stateT[] = ['ACTIVO', 'INACTIVO']
 
   //Funcion para controlar los inputs del filtro y actualizar el estado de los mismos
-  const handleChange = (key: keyof filterData, e: DropdownChangeEvent | EventTarget & HTMLInputElement) => {
-    setFilterState((prev) => ({...prev, [key]: e.value})) 
+  const handleChange = (
+    key: keyof filterData,
+    e: DropdownChangeEvent | (EventTarget & HTMLInputElement)
+  ) => {
+    setFilterState((prev) => ({ ...prev, [key]: e.value }))
   }
 
-  return <form  className={styles.filer_container}>
-    <section className={styles.inputs_container}>
-      <IconField value={username} iconPosition="left">
-        <InputIcon className="pi pi-search"> </InputIcon>
-        <InputText 
-          className='p-inputtext-lg' 
-          placeholder="Search" 
-          onChange={(e) => {handleChange('username',e.target)}}
+  return (
+    <form className={styles.filer_container}>
+      <section className={styles.input_container}>
+        <IconField value={username} iconPosition="left">
+          <InputIcon className="pi pi-search"> </InputIcon>
+          <InputText
+            className={`${styles.search} w-full`}
+            placeholder="Search"
+            onChange={(e) => {
+              handleChange('username', e.target)
+            }}
+          />
+        </IconField>
+      </section>
+      <section className={styles.input_container}>
+        <Dropdown
+          className={`${styles.search_dropdown} w-full h-full`}
+          placeholder="Selecciona el estado"
+          value={state}
+          onChange={(e) => {
+            handleChange('state', e)
+          }}
+          options={optEstado}
+          checkmark
+          showClear
         />
-      </IconField>
-      <Dropdown 
-        className="w-full md:w-14rem" 
-        placeholder='Selecciona el estado'
-        value={state}
-        onChange={(e) => {handleChange('state',e)}}
-        options={optEstado}
-        checkmark
-        showClear
-      />
-      <div>
+      </section>
+      <section className={styles.filter_btns}>
         <Link
           href={`?${new URLSearchParams({
             username: username ?? '',
-            state: state ?? ''
+            state: state ?? '',
           })}`}
         >
-          <IconButton iconClassname='pi pi-filter' withBackground />
+          <BaseButton
+            props={{
+              icon: 'pi pi-filter',
+              severity: 'secondary',
+            }}
+          ></BaseButton>
         </Link>
-        <IconButton onClick={(e) => {e.preventDefault()}} iconClassname='pi pi-sliders-v' withBackground/>
-      </div>
-    </section>
-  </form>
+        <BaseButton
+          onClick={(e) => {
+            e.preventDefault()
+          }}
+          props={{
+            icon: 'pi pi-sliders-v',
+            severity: 'secondary',
+          }}
+        />
+      </section>
+    </form>
+  )
 }
 
 export default Filter
