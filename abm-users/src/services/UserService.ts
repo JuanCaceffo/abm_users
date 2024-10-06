509353d8d7805ca9f251c1ab51b2cba2393292dd
JuanCaceffo <caceffojuan@gmail.com>
2024-10-06 04:37:45 -0300

    IMPR: little improovments in styles and identation

--- abm-users/src/services/UserService.ts
+++ abm-users/src/services/UserService.ts
@@ -1,19 +1,28 @@
-import { UserData } from "src/types/userTypes"
-import Service from "./config/service"
-import { User } from "src/models/User"
-import { pathsPrefix } from "./config/paths"
-import { stateT } from "src/types/filetTypes"
+import { UserData } from 'src/types/userTypes'
+import Service from './config/service'
+import { User } from 'src/models/User'
+import { pathsPrefix } from './config/paths'
+import { stateT } from 'src/types/filetTypes'
 
-class UserService extends Service{
-
-  async getAllUsers(username?: string, state?: stateT): Promise<User[]> {
+class UserService extends Service {
+  //TODO: Manejar mejor los parametros de filtrado
+  async getAllUsers(
+    username?: string,
+    state?: stateT,
+    page?: string,
+    limit?: string
+  ): Promise<User[]> {
     const params = {
-      usuario: username ? username : undefined , 
-      estado: state ? state : undefined
+      usuario: username ? username : undefined,
+      estado: state ? state : undefined,
+      _page: page ?? 1,
+      _limit: limit,
     }
 
-    const userData = (await this.httpClient.get<UserData[]>(`${pathsPrefix.user}`,{params})).data
-    return userData.map((userData) => (new User(userData)))
+    const userData = (
+      await this.httpClient.get<UserData[]>(`${pathsPrefix.user}`, { params })
+    ).data
+    return userData.map((userData) => new User(userData))
   }
 }
 
