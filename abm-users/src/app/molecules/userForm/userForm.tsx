fcc41eee1221e70f5954f579ae20e8c219047e6d
JuanCaceffo <caceffojuan@gmail.com>
2024-10-08 16:45:06 -0300

    FEAT: form error handilng

--- abm-users/src/app/molecules/userForm/userForm.tsx
+++ abm-users/src/app/molecules/userForm/userForm.tsx
@@ -5,8 +5,10 @@ import { InputText } from 'primereact/inputtext'
 import { FC } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
 //Helpers
 import { optSector, optState } from 'src/helpers/constants'
+import { userSchema } from 'src/helpers/userSchema'
 //Types
 import { UserData } from 'src/types/userTypes'
 //Styles
@@ -17,7 +19,6 @@ import BaseButton from 'src/app/atoms/baseButton/baseButton'
 import { userService } from 'src/services/UserService'
 
 //TODO: analizar si es mejor utilizar la enitadad de usuario para manejar errores y logica
-//TODO: manejar los erroes del form
 const UserForm: FC<{
   userData?: UserData
   onHide: () => void
@@ -27,14 +28,16 @@ const UserForm: FC<{
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<UserData>({ defaultValues: userData })
+  } = useForm<UserData>({
+    defaultValues: userData,
+    resolver: zodResolver(userSchema),
+  })
   const router = useRouter()
 
   const onSubmit: SubmitHandler<UserData> = (data) => {
     const response = userData
       ? userService.updateUser(data)
       : userService.createUser(data)
-
     response
       .then(() => {
         onHide()
@@ -49,45 +52,68 @@ const UserForm: FC<{
   const label = (title: string) => (
     <label className="font-semibold text-xl text-bluegray-700">{title}</label>
   )
+  const error = (error?: string) => (
+    <span className={`mt-1 text-red-600`} id="username-error" role="alert">
+      {error}
+    </span>
+  )
 
   return (
     <form className={`${styles.user_form}`} onSubmit={handleSubmit(onSubmit)}>
       <div className={`${styles.input_cnt}`}>
         {label('Id')}
-        <InputText
-          placeholder="Ingresar el id del Usuario"
-          type="number"
-          disabled={Boolean(userData)} //Deshabilitamos el input si el usuario ya existe
-          {...register('id', { required: true })}
-        ></InputText>
+        <div className="flex flex-column w-full">
+          <InputText
+            placeholder="Ingresar el id del Usuario"
+            type="number"
+            disabled={Boolean(userData)} //Deshabilitamos el input si el usuario ya existe
+            {...register('id', { valueAsNumber: true })}
+            invalid={Boolean(errors.id)}
+          ></InputText>
+          {error(errors.id?.message)}
+        </div>
       </div>
+
       <div className={`${styles.input_cnt}`}>
         {label('Usuario:')}
-        <InputText
-          placeholder="Ingresar el nombre del Usuario"
-          {...register('usuario')}
-        ></InputText>
+        <div className="flex flex-column w-full">
+          <InputText
+            placeholder="Ingresar el nombre del Usuario"
+            {...register('usuario')}
+            invalid={Boolean(errors.usuario)}
+          ></InputText>
+          {error(errors.usuario?.message)}
+        </div>
       </div>
+
       <div className={`${styles.input_cnt}`}>
         {label('Estado:')}
-        <Dropdown
-          placeholder="Seleccionar el estado"
-          value={watch('estado')}
-          options={optState}
-          {...register('estado')}
-          checkmark
-        ></Dropdown>
+        <div className="flex flex-column w-full">
+          <Dropdown
+            placeholder="Seleccionar el estado"
+            value={watch('estado')}
+            options={optState}
+            {...register('estado')}
+            checkmark
+          ></Dropdown>
+          {error(errors.estado?.message)}
+        </div>
       </div>
+
       <div className={`${styles.input_cnt}`}>
         {label('Sector:')}
-        <Dropdown
-          placeholder="Seleccionar el Sector"
-          value={+watch('sector')}
-          options={optSector}
-          {...register('sector')}
-          checkmark
-        ></Dropdown>
+        <div className="flex flex-column w-full">
+          <Dropdown
+            placeholder="Seleccionar el Sector"
+            value={+watch('sector')}
+            options={optSector}
+            {...register('sector', { valueAsNumber: true })}
+            checkmark
+          ></Dropdown>
+          {error(errors.sector?.message)}
+        </div>
       </div>
+
       <div className="flex justify-content-center gap-3 w-full">
         <BaseButton title="Confirmar" props={{ icon: 'pi pi-check' }} />
         <BaseButton
